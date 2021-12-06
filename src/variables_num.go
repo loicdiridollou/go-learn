42f8c9b94d630de77dae0af1abfec6d1d63b78e5
Loic Diridollou <loic.diridollou@gmail.com>
2021-12-06 18:41:15 -0500

    Float and complex types

--- src/variables_num.go
+++ src/variables_num.go
@@ -6,7 +6,21 @@ import (
 
 func main() {
 	var i float64 = 32.475
+	a := 10.2
+	b := 3.7
 
 	fmt.Printf("%v, %T", i, i)
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
 
+	var n complex64 = 3.3 + 2i
+	var m complex64 = 2 + 2.5i
+	fmt.Printf("%v, %T", n, n)
+	fmt.Println(n + m)
+	fmt.Println(n - m)
+	fmt.Println(n * m)
+	fmt.Println(n / m)
+	fmt.Println(real(n), imag(n))
 }
