11894f8483b3609dda2c56451f6a4e91c986764a
Loic Diridollou <loic.diridollou@gmail.com>
2021-12-06 08:32:10 -0500

    Adding variables learning

--- src/main.go
+++ src/main.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
+var (
+	actorName    string = "Elisabeth Sladen"
+	companion    string = "Sarah Jane Smith"
+	doctorNumber int    = 3
+	sason        int    = 11
+)
+
+var (
+	counter int = 0
+)
+
 func main() {
 	// variables
-	var i int = 12
-	var j int = 18
-	var k float64 = 24.
+	var i int = 42
+	fmt.Println(i)
+	i = 13
 
-	fmt.Printf("%v, %T, %T", i, j, k)
+	fmt.Println(i)
 }
