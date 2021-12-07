3f8c3789ee21c6e027f02759347c0a8d94948c2e
Loic Diridollou <loic.diridollou@gmail.com>
2021-12-07 15:04:33 -0500

    Adding formatting

--- src/constant_types.go
+++ src/constant_types.go
@@ -10,7 +10,22 @@ const (
 	snakeSpecialist
 )
 
+const (
+	_  = iota // ignore the first value by assigning to blank identifier
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
 func main() {
-	var specialistType int = dogSpecialist
-	fmt.Printf("%v", dogSpecialist == specialistType)
+	// var specialistType int = dogSpecialist
+	// fmt.Printf("%v", dogSpecialist == specialistType)
+	fileSize := 4000000000.
+	fmt.Printf("%.2f", fileSize)
+
 }
