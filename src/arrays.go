7cad31895ad1cf1b94d760327b815f7c45f97a0b
Loic Diridollou <loic.diridollou@gmail.com>
2021-12-16 21:58:59 -0800

    Continue lesson

--- src/arrays.go
+++ src/arrays.go
@@ -11,7 +11,27 @@ func main() {
 
 	fmt.Printf("Grades: %v, %v, %v\n", grade1, grade2, grade3)
 
-	grades := [3]int{97, 85, 93}
+	grades := [...]int{97, 85, 93}
 	fmt.Printf("Grades: %v\n", grades)
 
+	var students [5]string
+	fmt.Printf("Students: %v\n", students)
+	students[0] = "Lisa"
+	students[2] = "Ahmed"
+	students[1] = "John"
+	fmt.Printf("Student: %v\n", students[1])
+
+	a := [...]int{1, 2, 3}
+	b := &a
+	b[1] = 5
+	fmt.Println(a)
+	fmt.Println(b)
+
+	aa := [...]int{1, 2, 3}
+	fmt.Println(aa)
+	fmt.Println(len(aa))
+	fmt.Println(cap(aa))
+
+	c := make([]int, 3)
+	fmt.Println(c)
 }
