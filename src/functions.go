9bbc68a6cf502789139337e0f03d3c1d166f0255
Loic Diridollou <loic.diridollou@gmail.com>
2022-08-28 21:00:34 -0700

    Adding more content

--- src/functions.go
+++ src/functions.go
@@ -1,3 +1,4 @@
+// package implements some functions items
 package main
 
 import (
@@ -5,6 +6,10 @@ import (
 )
 
 func main() {
+	name := "Stacey"
+	greeting := "Hello "
+	sayGreeting(&greeting, &name)
+	fmt.Println(name)
 	for i := 0; i < 5; i++ {
 		sayMessage("Hello Go!", i)
 	}
@@ -13,4 +18,10 @@ func main() {
 func sayMessage(msg string, idx int) {
 	fmt.Println(msg)
 	fmt.Println("The value of index is ", idx)
+}
+
+func sayGreeting(greeting, name *string) {
+	fmt.Println(*greeting, *name)
+	*name = "Ted"
+	fmt.Println(*name)
 }
