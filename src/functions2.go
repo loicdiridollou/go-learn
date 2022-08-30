ab65f1f9f6bd0c5a5068f8123188c396aec562f8
Loic Diridollou <loic.diridollou@gmail.com>
2022-08-29 19:57:04 -0700

    Adding more content about the functions

--- src/functions2.go
+++ src/functions2.go
@@ -5,6 +5,12 @@ import "fmt"
 func main() {
 	d := sum(1, 2, 3, 4, 5)
 	fmt.Println(d)
+	dd, err := divide(5., 0.)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(dd)
 }
 
 func sum(values ...int) (result int) {
@@ -13,4 +19,11 @@ func sum(values ...int) (result int) {
 		result += v
 	}
 	return
+}
+
+func divide(a, b float64) (float64, error) {
+	if b == 0.0 {
+		return 0.0, fmt.Errorf("Cannot divide by 0")
+	}
+	return a / b, nil
 }
