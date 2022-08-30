f1969f7a24f7583da26e8cb5e6450dbba1df94e7
Loic Diridollou <loic.diridollou@gmail.com>
2022-08-29 20:31:10 -0700

    Adding passing struct by pointer

--- src/functions3.go
+++ src/functions3.go
@@ -3,7 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	func() {
-		fmt.Println("Hello go")
-	}()
+	g := greeter{
+		greeting: "Hello",
+		name:     "Go",
+	}
+	g.greet()
+	fmt.Println(g.name)
+	d := greeter{
+		greeting: "Hi",
+		name:     "John",
+	}
+	d.greet()
+}
+
+type greeter struct {
+	greeting string
+	name     string
+}
+
+func (g *greeter) greet() {
+	fmt.Println(g.greeting, g.name)
+	g.name = ""
 }
