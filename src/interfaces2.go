dd5d8ff3ca2c0be18b89b6969389a7f682988fa1
Loic Diridollou <loic.diridollou@gmail.com>
2022-09-01 22:05:29 -0700

    Adding type of interface

--- src/interfaces2.go
+++ src/interfaces2.go
@@ -5,8 +5,17 @@ import "fmt"
 func main() {
 	myInt := IntCounter(0)
 	var inc Incrementer = &myInt
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println(inc.Increment())
+	}
+	var i interface{} = 0
+	switch i.(type) {
+	case int:
+		fmt.Println("I am an int")
+	case string:
+		fmt.Println("I am a string")
+	default:
+		fmt.Println("I don't know")
 	}
 }
 
